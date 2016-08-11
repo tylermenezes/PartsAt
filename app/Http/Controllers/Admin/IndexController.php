28e84156445fd7ee92af0a3f492bc465d79505fa
Tyler Menezes <tylermenezes@gmail.com>
2016-08-10 19:43:24 -0700

    Add stats

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,13 +3,20 @@
 namespace PartsAt\Http\Controllers\Admin;
 
 use PartsAt\Services;
+use PartsAt\Models;
 use PartsAt\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller {
     public function getIndex()
     {
-        return \View::make('admin/index');
+        $stats = [
+            'unique' => Models\Part::count(),
+            'total' => Models\Part::sum('quantity'),
+            'value' => Models\Part::selectRaw('SUM(quantity * price_bulk) as value')->first()->value,
+            'unpriced' => Models\Part::where('price', '=', 0.0)->count()
+        ];
+        return \View::make('admin/index', ['stats' => $stats]);
     }
 
     public function getLogin()
